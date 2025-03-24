e8e6567f5c89f082817fef88a2b5524956da66a5
aryamraju <aryamraju686@gmail.com>
2025-03-24 09:28:09 +0530

    Update list.py

--- list.py
+++ list.py
@@ -6,5 +6,17 @@
 # print(thislist)
 
 
-thislist = ["apple", "banana", "cherry", "apple", "cherry"]
-print(thislist)
+
+# thislist = ["apple", "banana", "cherry", "apple", "cherry"]       #Example of allow duplicates
+# print(thislist)
+
+# thislist = list(("apple", "banana", "cherry")) # note the double round-brackets    #list() constructor
+# print(thislist)
+
+
+# thislist = ["apple", "banana", "cherry"]       #access items
+# print(thislist[1])
+
+
+# thislist = ["apple", "banana", "cherry"]       #negative indexing
+# print(thislist[-1])
