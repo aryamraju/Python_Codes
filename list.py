fe9cb52c22d9d5f3129086a952a0bbf03f03c75b
aryamraju <aryamraju686@gmail.com>
2025-03-24 09:20:39 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,10 @@
+# thislist = ["apple", "banana", "cherry"] #example of list
+# print(thislist)
+
+
+# thislist = ["apple", "banana", "cherry"]   #example of create a list
+# print(thislist)
+
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
