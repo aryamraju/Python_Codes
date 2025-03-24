3ec8c2f41f11d75e0b6547917b7cad11059b5de3
aryamraju <aryamraju686@gmail.com>
2025-03-24 10:00:57 +0530

    Add files via upload

---
+++ sortlist.py
@@ -0,0 +1,17 @@
+# Sort List Alphanumerically
+# List objects have a sort() method that will sort the list alphanumerically, ascending, by default:
+
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]     #Sort the list alphabetically:
+thislist.sort()
+print(thislist)
+
+
+
+# Sort Descending
+# To sort descending, use the keyword argument reverse = True:
+
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]        # Sort the list descending:
+thislist.sort(reverse = True)
+print(thislist)
